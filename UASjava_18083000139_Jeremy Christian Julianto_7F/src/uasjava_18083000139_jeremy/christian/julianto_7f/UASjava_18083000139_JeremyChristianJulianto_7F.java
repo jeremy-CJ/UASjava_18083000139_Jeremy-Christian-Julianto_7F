035369194860a6981e4322ee9decdcf2ebf943a7
Jeremy Christian Julianto <81346019+jeremy-CJ@users.noreply.github.com>
2022-01-16 14:55:32 +0700

    Add files via upload

---
+++ UASjava_18083000139_Jeremy Christian Julianto_7F/src/uasjava_18083000139_jeremy/christian/julianto_7f/UASjava_18083000139_JeremyChristianJulianto_7F.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uasjava_18083000139_jeremy.christian.julianto_7f;
+
+/**
+ *
+ * @author USER
+ */
+public class UASjava_18083000139_JeremyChristianJulianto_7F {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
